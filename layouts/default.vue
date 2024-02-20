beaa8c3df5815dad952b513ea43ab11962c2240d
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-20 22:19:19 +0300

    added the necessary libraries

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-	<section>
+	<section
+		class="grid"
+		style="min-height: 100vh"
+	>
+		<LayoutSidebar />
 		<slot></slot>
 	</section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.grid {
+	display: grid;
+	grid-template-columns: 1fr 4fr;
+}
+</style>
