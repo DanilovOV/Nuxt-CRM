eb0c32c38f4de10b60681f29aa5ef7bf4f999faa
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-20 23:23:10 +0300

    loader added

---
+++ components/layout/Loader.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="wrapper">
+		<NuxtImg
+			src="/loader.svg"
+			alt=""
+			width="220"
+		/>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+	@apply bg-sidebar flex items-center justify-center w-screen h-screen;
+}
+</style>
