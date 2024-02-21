6fe545313834d91bc96acdcaa0599411d121d80d
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-21 15:55:42 +0300

    added empty pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { type IColumn, type ICard } from '~/components/kanban/kanban.types'
+import { useKanbanQuery } from '~/components/kanban/useKanbanQuery'
+useSeoMeta({
+	title: 'Home | CRM',
+})
+
+const dragCardRef = ref<ICard | null>(null)
+const sourceColumnRef = ref<IColumn | null>(null)
+useKanbanQuery()
+</script>
 
 <template>
 	<div class="p-10">
 		<h1 class="font-bold text-2xl mb-10">Nuxt CRM system</h1>
+		<UiCard
+			class="mb-3"
+			draggable="true"
+		>
+			<UiCardHeader>123</UiCardHeader>
+			<UiCardContent>Компания</UiCardContent>
+			<UiCardFooter>Дата</UiCardFooter>
+		</UiCard>
 	</div>
 </template>
 
