c3bf36c87176abf23341e744775ca1ba9f7772d8
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-23 01:49:11 +0300

    ref: Loader

--- components/layout/Loader.vue
+++ components/layout/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="wrapper">
+	<div class="bg-sidebar flex items-center justify-center w-screen h-screen">
 		<NuxtImg
 			src="/loader.svg"
 			alt=""
@@ -7,9 +7,3 @@
 		/>
 	</div>
 </template>
-
-<style lang="scss" scoped>
-.wrapper {
-	@apply bg-sidebar flex items-center justify-center w-screen h-screen;
-}
-</style>
