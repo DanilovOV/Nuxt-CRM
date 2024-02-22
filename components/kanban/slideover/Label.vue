011013a4ab9e587bb12e460e9bc2e54668199467
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-23 02:33:17 +0300

    ref: props

--- components/kanban/slideover/Label.vue
+++ components/kanban/slideover/Label.vue
@@ -1,23 +1,24 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-const props = defineProps({
-	labelText: {
-		type: String,
-		default: '',
-	},
-})
+withDefaults(
+	defineProps<{
+		labelText: string
+	}>(),
+	{
+		labelText: '',
+	}
+)
 </script>
 
 <template>
 	<div class="mb-3">
-		<!-- Отображаем label, если он предоставлен в props -->
 		<label
 			v-if="labelText"
 			class="opacity-75 mb-0.5 block text-xs"
-			>{{ labelText }}</label
 		>
-		<!-- Слот для вложенного содержимого -->
+			{{ labelText }}
+		</label>
 		<div class="text-base"><slot /></div>
 	</div>
 </template>
