ed04c2ebd68b97c29bfe00cd337e129d6050471b
Олег Данилов <oleg-danilov8@ya.ru>
2024-02-23 02:35:00 +0300

    fix button props

--- components/ui/button/Button.vue
+++ components/ui/button/Button.vue
@@ -4,7 +4,7 @@ import { Primitive, type PrimitiveProps } from 'radix-vue'
 import { type ButtonVariants, buttonVariants } from '.'
 import { cn } from '@/utils'
 
-interface Props extends PrimitiveProps {
+interface Props extends /* @vue-ignore */ PrimitiveProps {
 	variant?: ButtonVariants['variant']
 	size?: ButtonVariants['size']
 	as?: string
